def6566996c9940866b766d46d13be8031c285ef
Kyle Jones <kjones65@ycp.edu>
2018-06-22 22:12:51 -0400

    Section 9: Lecture 139 (Didn't finish it in the last commit)

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,5 @@
-import axios from 'axios';
+import Search from './models/Search';
 
-async function getResults(query) {
-    const proxy = 'https://cors-anywhere.herokuapp.com/';
-    const key = '09e365692afcad2122c890f38949d9e5';
-    try {
-        const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${query}`);
-        const recipes = res.data.recipes;
-        console.log(recipes);
-    } catch (error){
-        alert(error);
-    }
-    
-}
-getResults('pizza');
+const search = new Search('pizza');
+console.log(search);
+search.getResults();
