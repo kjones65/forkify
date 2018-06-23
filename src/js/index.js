d8e1745fa75f424d63471102641619738beea251
Kyle Jones <kjones65@ycp.edu>
2018-06-22 22:00:37 -0400

    Section 9: Lectures 136-139

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,15 @@
-// Global app controller
-import num from './test';
-const x = 23;
-console.log(`I imported ${num} from another module called test.js! Variable x is ${x}`)
+import axios from 'axios';
+
+async function getResults(query) {
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    const key = '09e365692afcad2122c890f38949d9e5';
+    try {
+        const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${query}`);
+        const recipes = res.data.recipes;
+        console.log(recipes);
+    } catch (error){
+        alert(error);
+    }
+    
+}
+getResults('pizza');
